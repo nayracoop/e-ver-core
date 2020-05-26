be1afdd858040105d316d7fadcd22a457ac3f569
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-26 17:19:12 -0300

    Some components layout

--- src/containers/Event.js
+++ src/containers/Event.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Video from "./Video";
+import CommentList from "./CommentList";
+import EventDetails from "./EventDetails";
+import EventList from "./EventList";
+import SpeakerList from "./SpeakerList";
 
-const Event = ({props}) => {
+import { talks } from "../static-data";
 
-}
+const speakersList = (talks) => {
+  return talks.map((talk) => talk.speaker);
+};
+
+const Event = ({ props }) => {
+  const [selectedTalk, setSelectedTalk] = useState(talks[0]);
+  const selectTalk = (talk) => setSelectedTalk(talk);
+
+  return (
+    <>
+      <Video video={selectedTalk.video} />
+      <CommentList />
+      <EventDetails talk={selectedTalk} />
+      <EventList talks={talks} onSelectTalk={selectTalk} />
+      <SpeakerList speakersList={speakersList(talks)} />
+    </>
+  );
+};
 
 export default Event;
