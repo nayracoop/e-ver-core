cc0747bf422a72b559aa0404a76e83b85359f933
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-26 18:32:15 -0300

    Select lang implementation

---
+++ src/i18n/i18n.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import en from "./en/main.json";
+import es from "./es/main.json";
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: ["en", "es"],
+    lng: "en",
+    debug: true,
+    resources: { en, es },
+    joinArrays: "\n",
+    react: {
+      wait: true,
+    },
+  });
+
+console.log(i18n);
+
+export default i18n;
