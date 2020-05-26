3c6f66d4260f1439d3f97e5349529f327570eb69
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-26 17:25:57 -0300

    Format

--- src/static-data.js
+++ src/static-data.js
@@ -14,7 +14,7 @@ export const talks = [
       URI: "https://www.youtube.com/watch?v=j8NTPQR0lc8",
       comments: "Hola este es un comentario que tiene que ver con el video",
       autoplay: false,
-      type: "live"
+      type: "live",
     },
     speaker: {
       name: "Tulia Caradhro",
@@ -29,14 +29,15 @@ export const talks = [
     title: "Café, té y medialunas",
     details:
       "Excepteur sint occaecat cupidatat, dolore eu fugiat nulla pariatur non proident. Dolorem sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et fugiat nulla pariatur dolore magna aliqua. Irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    summary: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    begin: date.clone().add(4, 'hour').format(),
+    summary:
+      "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    begin: date.clone().add(4, "hour").format(),
     duration: 60,
     video: {
       URI: "https://www.youtube.com/watch?v=-smN6ZUHzgk",
       comments: "Hola este es un comentario que tiene que ver con el video",
       autoplay: false,
-      type: "recording"
+      type: "recording",
     },
     speaker: {
       name: "Paco Amd Plus",
