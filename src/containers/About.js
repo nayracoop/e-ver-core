87bb1730fbffbe0201881f4a4d2e47db06dad1f4
Pehuén <pehuen.rodriguez@nayra.coop>
2020-05-27 20:23:00 -0300

    Translated abouts

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const About = ({ props }) => {
-  return <div>Y tengo un about</div>;
+  const { t } = useTranslation();
+
+  return <div>{t("about")}</div>;
 };
 
 export default About;
